1b8bee41d24574f0cd166247551c51805ff02e40
matyo82 <matingarshasbii@gmail.com>
2023-06-30 23:50:25 +0330

    add temlates flask

--- blog/app.py
+++ blog/app.py
@@ -1,7 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route('/hi')
+def hi(name='matin'):
+    return render_template('hi.html',name=name)
 @app.route('/')
-def home():
-    return "welcome!!"
+def home(name='matin'):
+    return render_template('index.html',name=name)
