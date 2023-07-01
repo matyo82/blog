70beb07aebe288b519d3d93dd9070f8d0909aaa4
matyo82 <matingarshasbii@gmail.com>
2023-07-02 01:32:18 +0330

    add slask-sqlalchemy

--- blog/app.py
+++ blog/app.py
@@ -1,6 +1,16 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
+# create the app
 app = Flask(__name__)
+
+# create the extension
+db = SQLAlchemy()
+# configure the SQLite database, relative to the app instance folder
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
+# initialize the app with the extension
+db.init_app(app)
+
 
 @app.route('/hi')
 def hi(name='matin'):
